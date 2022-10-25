c0c48f8ca31bb85db1e2168f56bc4d05406acf66
techarm <techarmdev@gmail.com>
2022-10-26 00:16:27 +0900

    Finishing the Meal Detail Screen

---
+++ 03-navigation/01-meals-app/components/MealDetail/List.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const List = ({ data }) => {
+  return data.map((item) => (
+    <View style={styles.listItem} key={item}>
+      <Text style={styles.itemText}>{item}</Text>
+    </View>
+  ));
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginVertical: 8,
+    marginHorizontal: 12,
+    backgroundColor: "#e2b497",
+  },
+  itemText: {
+    color: "#351401",
+    // textAlign: "center",
+  },
+});
+
+export default List;
--- 03-navigation/01-meals-app/components/MealItem.js
+++ 03-navigation/01-meals-app/components/MealItem.js
@@ -7,6 +7,7 @@ import {
   Platform,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 const MealItem = ({ mealItem }) => {
   const navigation = useNavigation();
@@ -29,15 +30,11 @@ const MealItem = ({ mealItem }) => {
             <Image style={styles.image} source={{ uri: mealItem.imageUrl }} />
             <Text style={styles.title}>{mealItem.title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{mealItem.duration}m</Text>
-            <Text style={styles.detailItem}>
-              {mealItem.complexity.toUpperCase()}
-            </Text>
-            <Text style={styles.detailItem}>
-              {mealItem.affordability.toUpperCase()}
-            </Text>
-          </View>
+          <MealDetails
+            duration={mealItem.duration}
+            complexity={mealItem.complexity}
+            affordability={mealItem.affordability}
+          />
         </View>
       </Pressable>
     </View>

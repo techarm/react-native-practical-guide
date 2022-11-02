19446801b954b28f949b0c3fbfc8441159d9b046
techarm <techarmdev@gmail.com>
2022-11-02 21:51:00 +0900

    Storing auth tokens on the device & Logging users in automatically

--- 08-authentication/App.js
+++ 08-authentication/App.js
@@ -1,14 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import IconButton from './components/ui/IconButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
@@ -67,12 +69,34 @@ function Navigation() {
   );
 }
 
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    const fetchToken = async () => {
+      const storedToken = await AsyncStorage.getItem('token');
+      if (storedToken) {
+        authContext.authenticate(storedToken);
+      }
+
+      setIsTryingLogin(false);
+    };
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <AppLoading />;
+  }
+
+  return <Navigation />;
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
--- 08-authentication/store/auth-context.js
+++ 08-authentication/store/auth-context.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({
   token: '',
@@ -12,10 +13,12 @@ function AuthContextProvider({ children }) {
 
   const authenticateHandler = (token) => {
     setAuthToken(token);
+    AsyncStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setAuthToken(null);
+    AsyncStorage.removeItem('token');
   };
 
   const value = {

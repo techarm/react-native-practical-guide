5d13841142db1dc24f28f118cf0343df69e33efa
techarm <100292270+techarm@users.noreply.github.com>
2022-11-05 19:31:52 +0900

    Update location.js

--- 09-native-device-features/utils/location.js
+++ 09-native-device-features/utils/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyAGOOvI6ETGC5EfT-7ufCighCFqed98eHs';
+const GOOGLE_API_KEY = 'YOUR-API-KEY';
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
